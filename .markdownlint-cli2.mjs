7a31929adff3415bd14db57003ca10ef78911b62
Jaakko Mustalahti <jaska.mustalahti@gmail.com>
2024-10-16 16:21:27 +0300

    disable certain lint rules

--- .markdownlint-cli2.mjs
+++ .markdownlint-cli2.mjs
@@ -2,6 +2,9 @@ export default {
   config: {
     default: true,
     "MD013": false,
-    "MD033": false
+    "MD033": false,
+    "MD034": false,
+    "MD001": false, // TODO remove and enforce the rule
+    "MD036": false, // TODO remove and enforce the rule
   }
 }
